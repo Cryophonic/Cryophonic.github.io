9c37b793e030118069d01b7d04fe12f7c43546f5
Cyril Domingo <96261888+Cryophonic@users.noreply.github.com>
2021-12-17 21:42:59 +0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+// javascript
+
+function projects(){
+    var elmnt = document.getElementById("project");
+    elmnt.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+
+function about(){
+    var elmnt = document.getElementById("about");
+    elmnt.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+
+function contact(){
+    var elmnt = document.getElementById("contact");
+    elmnt.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+
+
+
